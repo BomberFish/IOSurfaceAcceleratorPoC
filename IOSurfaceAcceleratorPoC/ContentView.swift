ad03b5b75feff679054230c0e57d449e5d2fa77c
BomberFish <87151697+BomberFish@users.noreply.github.com>
2023-04-12 13:39:06 -0400

    the travelers example to the cosmos

--- IOSurfaceAcceleratorPoC/ContentView.swift
+++ IOSurfaceAcceleratorPoC/ContentView.swift
@@ -9,14 +9,15 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-            Button("panic", action: {trigger_memmove_oob_copy()})
+        NavigationView {
+            VStack {
+                Button("Don't panic", action: {trigger_memmove_oob_copy()})
+                    .tint(.red)
+                    .buttonStyle(.bordered)
+            }
+            .padding()
+            .navigationTitle("IOSurfaceAccelerator PoC")
         }
-        .padding()
     }
 }
 
